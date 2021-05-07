7f1c4ca1cd464aea8f28b9f69704e261229e720b
Dennis <dennisjames2896@gmail.com>
2021-05-07 12:03:28 +0800

    updated

--- techtrek/src/LoginPage.js
+++ techtrek/src/LoginPage.js
@@ -3,6 +3,8 @@ import { useHistory } from "react-router-dom";
 import Header from "./components/Header";
 
 function LoginPage() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
   const history = useHistory();
 
   useEffect(() => {
@@ -10,6 +12,10 @@ function LoginPage() {
       history.push("/add");
     }
   }, []);
+
+  function login() {
+    console.warn(email, password);
+  }
 
   return (
     <div>
@@ -19,24 +25,33 @@ function LoginPage() {
         <input
           type="text"
           placeholder="Email Address"
+          onChange={(e) => setEmail(e.target.value)}
           className="form-control"
         ></input>
 
         <br />
         <input
-          type="text"
+          type="password"
           placeholder="Password"
+          onChange={(e) => setPassword(e.target.value)}
           className="form-control"
         ></input>
         <br />
-
         <input
           type="checkbox"
           placeholder="Remember me"
           className="form-control"
         ></input>
+        <br />
+        <button
+          onClick={login}
+          className="btn btn-primary"
+          width="300"
+          height="300"
+        ></button>
       </div>
     </div>
   );
 }
+
 export default LoginPage;
