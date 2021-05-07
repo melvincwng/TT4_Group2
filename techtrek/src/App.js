471e6b40fce6b8813ac87622e0a08977e20d9b62
t-yanshinvernon <vernon1006@gmail.com>
2021-05-07 12:07:03 +0800

    Updated App.js with history.

--- techtrek/src/App.js
+++ techtrek/src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import NavigationBar from "./components/NavigationBar";
+import TransactionHistory from "./components/TransactionHistory"
 import "bootstrap/dist/css/bootstrap.min.css";
 
 export default function App() {
@@ -12,7 +13,7 @@ export default function App() {
           <Route path="/" exact component={() => <div>Homepage</div>} />
           <Route path="/login" exact component={() => <div>Login Page</div>} />
           <Route path="/transfer" exact component={() => <div>Transfer</div>} />
-          <Route path="/history" exact component={() => <div>History</div>} />
+          <Route path="/history" exact component={() => <TransactionHistory/>} />
           <Route path="/balance" exact component={() => <div>Balance</div>} />
           <Route path="/chat" exact component={() => <div>Chat</div>} />
         </Switch>
