fb6b8e03d9aa60617f5ca632b926236e173482ef
t-yanshinvernon <vernon1006@gmail.com>
2021-05-07 16:27:14 +0800

    Reverted merge conflict

--- techtrek/src/components/TransactionHistory.js
+++ techtrek/src/components/TransactionHistory.js
@@ -1,69 +1,58 @@
-import React from "react";
-import axios from "axios";
-import { useState, useEffect } from "react";
+import React from 'react'
+import axios from "axios"
+import { useState, useEffect } from "react"
 import { Table } from "react-bootstrap";
-import NavigationBar from "./NavigationBar";
 /*
  * User must be able to view his/her own transaction history.
  * Data visualization through simple graphs.
  * View Transaction Details - /POST
  * https://ipllrj2mq8.execute-api.ap-southeast-1.amazonaws.com/techtrek/transactions/view
  * Retrieves transaction information.
- */
+*/
 
 function TransactionHistory() {
-  // /POST configurations
-  // API for view transaction details
-  const transactionAPI =
-    "https://ipllrj2mq8.execute-api.ap-southeast-1.amazonaws.com/techtrek/transactions/view";
 
-  // JSON Body containing the customer Id and account key
-  const credentials = {
-    custID: 2,
-    accountKey: "4jzir16n-izjd-ytll-cjlp-u8i5ney8lkx",
-  };
+    // /POST configurations
+    // API for view transaction details
+    const transactionAPI = 'https://ipllrj2mq8.execute-api.ap-southeast-1.amazonaws.com/techtrek/transactions/view';
 
-  // Config containing headers for the api key.
-  const config = {
-    headers: {
-      "x-api-key": "PXeFPCR73iU4khaSqcRM1KBX5SblxPm4R1Rzt07a",
-    },
-  };
-
-  // States
-  const [transactionHistory, setTransactionHistory] = useState([]);
-  const [tableInfo, setTableInfo] = useState([]);
-  
-
-  
-
-  useEffect(() => {
-    const fetchTransactionHistory = async () => {
-        const response = axios
-          .post(transactionAPI, credentials, config)
-          .then((response) => {
-            setTransactionHistory(response.data);
-          })
-          .catch((err) => {
-            console.log(err);
-          });
-    
-        return response;
-      };
-    const convertTransactionHistoryDateTime = () => {
-        let data = [];
-        for (var i in transactionHistory) {
-            let val = transactionHistory[i];
-            let unix_timestamp = val.datetime;
-            var date = new Date(unix_timestamp * 1000);
-            data.push({ ...val, datetime: date });
-        }
-    setTableInfo(data);
+    // JSON Body containing the customer Id and account key
+    const credentials = {
+        'custID': 2,
+        'accountKey': '4jzir16n-izjd-ytll-cjlp-u8i5ney8lkx'
     };
-    fetchTransactionHistory();
-    convertTransactionHistoryDateTime();
-    }, []);
 
+    // Config containing headers for the api key.
+    const config = {
+        headers: {
+            'x-api-key' : 'PXeFPCR73iU4khaSqcRM1KBX5SblxPm4R1Rzt07a'
+        }
+    }
+
+    // States
+    const [transactionHistory, setTransactionHistory] = useState([]);
+
+    const fetchTransactionHistory = async () => {
+        const response = axios.post(
+            transactionAPI,
+            credentials,
+            config
+            ).then((response) => {
+                console.log(response);
+                setTransactionHistory(response.data);
+                }
+            ).catch((err) => {
+                console.log(err);
+                }
+            );
+            
+        return response;
+    }
+
+    useEffect(() => {
+        fetchTransactionHistory();
+    },[]);
+    
     return (
         <>
             <h1>Transaction History</h1>
@@ -75,16 +64,16 @@ function TransactionHistory() {
                         <th>Amount</th>
                         <th>e-Gift</th>
                         <th>Expenses</th>
-                        <th>Message</th>
+                        <th>message</th>
                         <th>Payee Id</th>
                     </tr>
                 </thead>
                 <tbody>
-                    {tableInfo.map((transactionHistory, index) => (
+                    {transactionHistory.map((transactionHistory, index) => (
                     <tr key={index}>
                     <td>{transactionHistory.custID}</td>
                     <td>{transactionHistory.datetime}</td>
-                    <td>{(transactionHistory.payeeID === credentials.custID) ? `+${transactionHistory.amount}` : `-${transactionHistory.amount}`}</td>
+                    <td>{(transactionHistory.payeeID == credentials.custID) ? `+${transactionHistory.amount}` : `-${transactionHistory.amount}`}</td>
                     <td>{JSON.stringify(transactionHistory.eGift)}</td>
                     <td>{transactionHistory.expenseCat}</td>
                     <td>{transactionHistory.message}</td>
@@ -97,4 +86,4 @@ function TransactionHistory() {
     )
 }
 
-export default TransactionHistory;
+export default TransactionHistory
