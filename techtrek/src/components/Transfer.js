4ba5ac6cbe7ddf43137579e129f99e70c33372a7
jazreel <jazreel.kwek@gmail.com>
2021-05-07 13:09:55 +0800

    bootstrap form

--- techtrek/src/components/Transfer.js
+++ techtrek/src/components/Transfer.js
@@ -1,21 +1,32 @@
 import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import '../App.css';
 
 export default function transfer() {
   return (
     <form>
-        Amount:
-        <input type="text" name="name" />
-        <br/><br/>
-        Pay to:
-        <input type="text" name="payto" />
-        <br/><br/>
-        Message:
-        <input type="text" name="message" />
-        <br/><br/>
-        Is this an eGift?:
-        <input type="checkbox" name="egift"/>
-        <br/><br/>
-        <input type="submit" value="Submit" />
+        <div class="mx-auto Form-centered" style={{width: 50 + '%'}}>
+            <label class="sr-only" for="amount">Amount</label>
+            <div class="input-group mb-2 mr-sm-2">
+                <div class="input-group-prepend">
+                <div class="input-group-text">$</div>
+                </div>
+                <input type="text" class="form-control" id="amount" placeholder="Amount" />
+            </div>
+
+            <div class="form-group">
+                <label for="name">Name</label>
+                <input type="text" class="form-control" id="name" placeholder="Name"/>
+            </div>
+
+            <br/>
+            <div class="form-check">
+                <input type="checkbox" class="form-check-input" id="egift"/>
+                <label class="form-check-label" for="exampleCheck1">E-Gift</label>
+            </div>
+
+            <button type="submit" class="btn btn-primary">Submit</button>
+        </div>
     </form>
   );
 }
